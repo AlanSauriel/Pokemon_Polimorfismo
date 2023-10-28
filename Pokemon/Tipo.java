95a915e2acf019f10c22438d7e2245b0770656d8
AlanSauriel <132103150+AlanSauriel@users.noreply.github.com>
2023-10-27 23:06:09 -0600

    Tipo.java

--- Pokemon/Tipo.java
+++ Pokemon/Tipo.java
@@ -1,1 +1,9 @@
-tipo
+package pokemon.pokemon;
+public enum Tipo {
+    ACERO,AGUA, BICHO,
+    DRAGON, ELECTRICO, FANTASMA,
+    FUEGO, HADA, HIELO,
+    LUCHA, NORMAL, PLANTA,
+    PSIQUICO, ROCA, SINIESTRO,
+    TIERRA, VENENO, VOLADOR
+}
