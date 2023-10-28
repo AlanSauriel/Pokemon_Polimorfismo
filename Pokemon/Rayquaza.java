9748e31a74138ae30256fef70a11bf06ab82c975
AlanSauriel <132103150+AlanSauriel@users.noreply.github.com>
2023-10-27 23:04:34 -0600

    Rayquaza.java

--- Pokemon/Rayquaza.java
+++ Pokemon/Rayquaza.java
@@ -1,7 +1,7 @@
 package pokemon.pokemon;
 
-public class Articuno extends Pokemon {
-    public Articuno(Tipo tipo, int PS, int Ataque, int Defensa, int AtaqueEspecial, int DefensaEspecial, int Velocidad, int Nivel) {
+public class Rayquaza extends Pokemon {
+    public Rayquaza(Tipo tipo, int PS, int Ataque, int Defensa, int AtaqueEspecial, int DefensaEspecial, int Velocidad, int Nivel) {
         super(Tipo.DRAGON.VOLADOR, 105, 150, 90, 150, 90, 95,10);
 
         ListaMovimientos listaMovimientos = new ListaMovimientos();
@@ -20,17 +20,19 @@ public class Articuno extends Pokemon {
         double efectividad = 1.0;
 
         //No efectivo
-        if (pokemon.getTipo() == Tipo.ROCA) efectividad = 0.0;
+        if (pokemon.getTipo() == Tipo.ROCA) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.DRAGON) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.HADA) efectividad = 0.50;
+        
         
         //No muy efectivo
-        if (pokemon.getTipo() == Tipo.ACERO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.ELECTRICO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.FUEGO) efectividad = 0.25;
-        if (pokemon.getTipo() == Tipo.BICHO) efectividad = 0.50;
-        if (pokemon.getTipo() == Tipo.PLANTA) efectividad = 0.50;
+        if (pokemon.getTipo() == Tipo.AGUA) efectividad = 1.0;
+        if (pokemon.getTipo() == Tipo.FUEGO) efectividad = 1.0;
+        if (pokemon.getTipo() == Tipo.BICHO) efectividad = 1.0;
+        if (pokemon.getTipo() == Tipo.LUCHA) efectividad = 1.00;
         
         //Muy efectivo
-        if (pokemon.getTipo() == Tipo.TIERRA) efectividad = 4.0;
+        if (pokemon.getTipo() == Tipo.PLANTA) efectividad = 4.0;
        
         return efectividad;     
     }
